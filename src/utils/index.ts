c43515ff6a107e2c3d0849ccee2dc6fa5b01404b
Alex <alexheppell@protonmail.com>
2025-01-21 00:10:35 +0000

    create transform util

---
+++ src/utils/index.ts
@@ -0,0 +1,16 @@
+import { Point } from "pixi.js";
+
+export function transformPoints(points: Array<Point>, scale: number, rot: number): Array<Point> {
+    const cosTheta = Math.cos(rot);
+    const sinTheta = Math.sin(rot);
+
+    return points.map(( point ) => {
+        const scaledX = point.x * scale;
+        const scaledY = point.y * scale;
+
+        const transformedX = scaledX * cosTheta - scaledY * sinTheta;
+        const transformedY = scaledX * sinTheta + scaledY * cosTheta;
+
+        return new Point(transformedX, transformedY);
+    });
+}
