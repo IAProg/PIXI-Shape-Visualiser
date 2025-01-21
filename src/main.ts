7b0eb67b52b581c48ac0b8aba0df22337e2134f7
Alex <alexheppell@protonmail.com>
2025-01-21 01:51:48 +0000

    load shape data on launch

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { App } from "./application";
 import { loadAssets } from "./asset-loader";
+import { dataModel } from "./dataModel";
 
 
 // simple bootstrap load assets -> start app 
 // pixiapp is registered for pixi inspector
-loadAssets().then(() => {
-    const app = new App();
-    (globalThis as any).__PIXI_APP__ = app;
-    document.body.appendChild(app.view)
-});
+dataModel.init().then(() => 
+    loadAssets().then(() => {
+        const app = new App();
+        (globalThis as any).__PIXI_APP__ = app;
+        document.body.appendChild(app.view)
+    })
+);
+
+
